707b0bc438314c13d3e47ac6b54522bf4fc28ce8
MatDevYou <cod4882@gmail.com>
2024-05-07 15:05:57 +0200

    Aggiungere i file di progetto.

---
+++ ClasseEmail/Email.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+
+namespace ClasseEmail
+{
+    internal class Email
+    {
+        public string Da { get; set; }
+        public string A { get; set; }
+        public string CC { get; set; }
+        public string BCC { get; set; }
+        public string Messaggio { get; set; }
+        public DateTime Data { get; set; }
+        public Priorita Priorita { get; set; }
+
+        public Email() // Costruttore predefinito
+        {
+            // Imposta valori predefiniti
+            Data = DateTime.Now;
+            Priorita = Priorita.NORMALE;
+        }
+
+        public Email(string da, string a, string cc, string bcc, string messaggio, DateTime data, Priorita priorita)
+        {
+            Da = da;
+            A = a;
+            CC = cc;
+            BCC = bcc;
+            Messaggio = messaggio;
+            Data = data;
+            Priorita = priorita;
+        }
+
+        public string DeterminaPriorita()
+        {
+            // Definisce il comportamento in base alla priorità
+            switch (Priorita)
+            {
+                case Priorita.BASSA:
+                    return "Azioni a bassa priorità";
+                case Priorita.ALTA:
+                    return "Azioni ad alta priorità";
+                default:
+                    return "Azioni a priorità normale";
+            }
+        }
+
+         public string Stampa()
+        {
+            return $"" +
+                $"\nDa: {Da}" +
+                $"\nA: {A}" +
+                $"\nCC: {CC}" +
+                $"\nBCC: {BCC}" +
+                $"\nMessaggio: {Messaggio}" +
+                $"\nData: {Data}" + 
+                $"\nPriorita: {Priorita}" +
+                $"\nFrase: {DeterminaPriorita()}" 
+                ;
+        }
+    }
+
+    public enum Priorita
+    {
+        BASSA,
+        NORMALE,
+        ALTA
+    }
+}
---
+++ ClasseEmail/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace ClasseEmail
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Esercizio - ClasseEmail
+             Si consideri di voler gestire un email. Sono richiesti i seguenti dati: DA, A, CC, BCC, Oggetto, Messaggio, Data (default), 
+            Priorità  con i valori: BASSA, NORMALE (default) o ALTA. Si ipotizzi che nei campi DA, A CC e BCC si possa inserire solo un indirizzo email.
+
+             Si richiede di creare tre istanze, assegnando per ognuna una priorità differente.
+             */
+
+            Email email = new Email
+            {
+                Da = "io@gmail.com",
+                A = "destinatario@gmail.com",
+                CC = "copia@gmail.com",
+                BCC = "copiaNascosta@gmail.com",
+                Messaggio = "Mex prova.",
+                Data = DateTime.Now,
+                Priorita = Priorita.ALTA
+            };
+
+            Email email1 = new Email
+            {
+                Da = "io@gmail.com",
+                A = "destinatario@gmail.com",
+                CC = "copia@gmail.com",
+                BCC = "copiaNascosta@gmail.com",
+                Messaggio = "Mex prova.",
+                Data = DateTime.Now,
+                Priorita = Priorita.BASSA
+            };
+
+            Email email2 = new Email
+            {
+                Da = "io@gmail.com",
+                A = "destinatario@gmail.com",
+                CC = "copia@gmail.com",
+                BCC = "copiaNascosta@gmail.com",
+                Messaggio = "Mex prova.",
+                Data = DateTime.Now,
+                Priorita = Priorita.NORMALE
+            };
+
+            // Stampa dell'email
+            Console.WriteLine(email.Stampa());
+            Console.WriteLine(email1.Stampa());
+            Console.WriteLine(email2.Stampa());
+
+
+
+
+            // Determina azioni in base alla priorità
+
+        }
+    }
+}
